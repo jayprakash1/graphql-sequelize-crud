3f1cfb5b72843f4fa228cb61d89901a4198d8c8b
jay prakash <jay.prakash1@gmail.com>
2020-09-04 12:56:49 +0530

    changes to fix delete mutation fat query

--- src/index.js
+++ src/index.js
@@ -671,7 +671,7 @@ function _deleteRecord({
       convertFieldsFromGlobalId(Model, where);
       let values = {};
       return Model.findOne({ where: where, requestUser: context.user} ).then((instance) => {
-        values = convertFieldsToGlobalId(instance.dataValues);
+        values = convertFieldsToGlobalId(Model, instance.dataValues);
       }).then(() => {
         return Model.destroy({
           where,
